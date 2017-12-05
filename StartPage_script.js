b63e39ade2eaa67350310104a1186bb85b648980
mareise <maxi.reisecker@gmail.com>
2017-12-05 15:26:35 +0000

    Änderung

--- StartPage_script.js
+++ StartPage_script.js
@@ -1,11 +1,1 @@
-function myFunction_close() {
-    document.getElementById("Warning").style.display="display: none";
 
-    console.log('Seas');
-};
-
-function myFunction_close() {
-    document.getElementById("Success").style.display="display: none";
-
-    console.log('Seas');
-}
