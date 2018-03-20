f7dd1920a70f4a36a38ef879bade9e1a898b0843
Mareise <34275515+Mareise@users.noreply.github.com>
2018-03-20 10:18:52 +0100

    eingeben der Werte

--- Server.js
+++ Server.js
@@ -36,9 +36,9 @@ var encoder = new RotaryEncoder({
 var result
 
 let getränkObj = [
-{id: 0, getränk:"malibu", position:"10"},
-{id: 1, getränk:"rum", position:"-10"},
-{id: 2, getränk:"wodka", position:"-20"}
+{id: 0, getränk:"malibu", position:"-120"},
+{id: 1, getränk:"rum", position:"30"},
+{id: 2, getränk:"wodka", position:"226"}
 ];
 
 // --------------------------------------------------------------------------
